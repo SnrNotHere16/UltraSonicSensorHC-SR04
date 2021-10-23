21ff7e43b3a34e6ea06b096b3f9ccf9464087739
SnrNotHere16 <aquineda16@hotmail.com>
2021-10-23 00:37:54 -0700

    Timer3 completed (Do not start from here)
    Timer3 completed Do not start from here. Code is messy

--- UltraSonicSensorTimer/hc-sr04.c
+++ UltraSonicSensorTimer/hc-sr04.c
@@ -9,9 +9,9 @@ const double _16MHz_1clock = 0.0625; // Value of 1clock cycle in mikroseconds
  
 #define ECHO0 0x40 //PB6
 #define TRIGGER0 0x40 //PA6(OUTPUT)
-#define ECHO1 0x80 //PB7
+#define ECHO1 0x01 //PB7
 #define TRIGGER1 0x20 //PA5
-#define ECHO2 0x01 //PB0
+#define ECHO2 0x04 //PB2
 #define TRIGGER2 0x10 //PA4
 #define BLUE_LED 0x04 //PF2 onboard Blue LED
 #define GREEN_LED 0x08 //PF3 onboard Green LED
@@ -26,13 +26,11 @@ int32_t measureDistanceOnce(void) {
     GPIO_PORTA_DATA_R |= TRIGGER0;
     delay_Microsecond(12);
     GPIO_PORTA_DATA_R &=~TRIGGER0;
-		 //UART_OutString("Stuck Before zero while loop\n"); 
     /*Capture firstEgde i.e. rising edge*/
     TIMER0_ICR_R =4;
 		
     while((TIMER0_RIS_R & 4)==0){}; //Wait till captured
 		highEdge =  TIMER0_TAR_R;
-		//UART_OutString("Stuck after zero while loop\n"); 
 		/*Capture secondEdge i.e. falling edge */
 		TIMER0_ICR_R =4; //clear timer capture flag
 		while((TIMER0_RIS_R & 4)  ==0){};
@@ -83,6 +81,37 @@ int32_t measureDistanceOnce1(void) {
     return ddistance;
 }
 
+int32_t measureDistanceOnce2(void) { 
+		uint32_t highEdge,lowEdge;
+		int32_t ddistance; /*Distance in centimeters*/
+    double travelTime = 0;
+    GPIO_PORTA_DATA_R &=~TRIGGER0;
+    delay_Microsecond(12);
+    GPIO_PORTA_DATA_R |= TRIGGER0;
+    delay_Microsecond(12);
+    GPIO_PORTA_DATA_R &=~TRIGGER0;
+    /*Capture firstEgde i.e. rising edge*/
+    TIMER3_ICR_R =4;
+    while((TIMER3_RIS_R & 4)==0){}; //Wait till captured
+		highEdge =  TIMER3_TAR_R;
+
+		/*Capture secondEdge i.e. falling edge */
+		TIMER3_ICR_R =4; //clear timer capture flag
+		while((TIMER3_RIS_R & 4)  ==0){};
+		lowEdge = TIMER3_TAR_R;
+		travelTime = (lowEdge - highEdge) * _16MHz_1clock;
+		if (travelTime < 58) {
+			ddistance = -1;
+		}
+		else if (travelTime > 11662) {
+			ddistance = 9999;
+		}
+		else {
+		  ddistance = round((travelTime * 0.5) * 0.0343);
+		}
+
+    return ddistance;
+}
 
 int32_t measureD() {
    uint32_t counter = 0;
@@ -91,8 +120,8 @@ int32_t measureD() {
 	
 	 for (counter = 0; counter < 100; ++counter) {
 		 //instantDist = measureDistanceOnce();
-	   instantDist = measureDistanceOnce1(); 
-		 //instantDist = measureDistanceOnce2();
+	 // instantDist = measureDistanceOnce1(); 
+		 instantDist = measureDistanceOnce2();
 		 if (instantDist > 0 && instantDist < 9999) {
 		    dist = (dist + instantDist) / 2;
 		 }
@@ -109,6 +138,7 @@ void InitRegisters(){
     GPIO_PORTF_DIR_R |= RED_LED | GREEN_LED | BLUE_LED;
     GPIO_PORTA_DEN_R |= TRIGGER0;
 	  GPIO_PORTB_DEN_R |= ECHO0;
+	  GPIO_PORTB_DEN_R |= ECHO1; 
 		GPIO_PORTB_DEN_R |= ECHO2; 
     GPIO_PORTF_DEN_R |= RED_LED | GREEN_LED | BLUE_LED;
 }
@@ -173,31 +203,15 @@ void Timer0A_init(void)
     TIMER0_CTL_R |= 1;
 }
 
-void Timer0B_init(void){
-	//PB7
-	  SYSCTL_RCGCTIMER_R |= 0x01;  //pg 338
-    SYSCTL_RCGCGPIO_R |= 0x02; 
-    GPIO_PORTB_DIR_R &= ~ECHO1;
-    GPIO_PORTB_DEN_R |=ECHO1;
-    GPIO_PORTB_AFSEL_R |=ECHO1;
-    GPIO_PORTB_PCTL_R &= ~0xF0000000; //pg 668
-    GPIO_PORTB_PCTL_R |= 0x70000000; //pg 668
- 
-    TIMER0_CTL_R &= ~1; //pg 737 disable control register 
-    TIMER0_CFG_R = 4; //pg 727 set 16 bit timer
-    TIMER0_TBMR_R = 0x17; // pg 733 Capture mode, Edge Time mode, Timer count up
-    TIMER0_CTL_R |= 0x0C; //pg 737 Both Edges
-    TIMER0_CTL_R |= 1; //pg 737 Enable
-	
-}
+
 
 void Timer2A_init(void){
 	//PB0
 	  SYSCTL_RCGCTIMER_R |= 0x04;  //pg 338
     SYSCTL_RCGCGPIO_R |= 0x02; 
-    GPIO_PORTB_DIR_R &= ~ECHO2;
-    GPIO_PORTB_DEN_R |=ECHO2;
-    GPIO_PORTB_AFSEL_R |= ECHO2;
+    GPIO_PORTB_DIR_R &= ~ECHO1;
+    GPIO_PORTB_DEN_R |=ECHO1;
+    GPIO_PORTB_AFSEL_R |= ECHO1;
     GPIO_PORTB_PCTL_R &= ~0x0000000F; //pg 668
     GPIO_PORTB_PCTL_R |= 0x00000007; //pg 668
  
@@ -206,4 +220,21 @@ void Timer2A_init(void){
     TIMER2_TAMR_R = 0x17; // pg 733 Capture mode, Edge Time mode, Timer count up
     TIMER2_CTL_R |= 0x0C; //pg 737 Both Edges
     TIMER2_CTL_R |= 1; //pg 737 Enable
+}
+
+void Timer3A_init(void){
+	//PB7
+	  SYSCTL_RCGCTIMER_R |= 0x08;  //pg 338
+    SYSCTL_RCGCGPIO_R |= 0x02; 
+    GPIO_PORTB_DIR_R &= ~ECHO2;
+    GPIO_PORTB_DEN_R |=ECHO2;
+    GPIO_PORTB_AFSEL_R |=ECHO2;
+    GPIO_PORTB_PCTL_R &= ~0x00000F00; //pg 668
+    GPIO_PORTB_PCTL_R |= 0x00000700; //pg 668
+ 
+    TIMER3_CTL_R &= ~1; //pg 737 disable control register 
+    TIMER3_CFG_R = 4; //pg 727 set 16 bit timer
+    TIMER3_TAMR_R = 0x17; // pg 733 Capture mode, Edge Time mode, Timer count up
+    TIMER3_CTL_R |= 0x0C; //pg 737 Both Edges
+    TIMER3_CTL_R |= 1; //pg 737 Enable
 }
