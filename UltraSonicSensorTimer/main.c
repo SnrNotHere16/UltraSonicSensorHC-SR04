a809db480a3d44ad43540e4ad82df2d621beae05
SnrNotHere16 <aquineda16@hotmail.com>
2021-10-24 17:07:51 -0700

    Added pinout description to main.c
    main.c has comments detailing the pinouts for the
    1. Sensors
    2. UART
    3. Onboard LEDS

--- UltraSonicSensorTimer/main.c
+++ UltraSonicSensorTimer/main.c
@@ -3,7 +3,27 @@
 #include "UART.h"
 #include "PLL.h"
 // user button connected to PF4 (increment counter on falling edge)
-
+/*
+Pins 
+Sensor Pins --
+	Sensor 0 - 
+		ECHO0 - PB6 (Timer0A)
+		TRIGGER0 - PA6
+	Sensor 1 -
+		ECHO1 - PB0 (Timer2A)
+		TRIGGER1 - PA5
+	Sensor 2 - 
+		ECHO2 - PB2 (Timer3A)
+		TRIGGER2 - PA4
+UART PINS -- 
+	UART0 -- 
+		PA0 - UART0 RX
+		PA1 - UART0 TX
+ONBOARD LEDS -- 
+	PF1 - red (0x02) 
+	PF2 - blue (0x04) 
+	PF3 - green (0x08) 
+*/
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
@@ -20,7 +40,6 @@ int main(void){
 	char distStr0[20] = "";
 	char distStr1[20] = "";
 	char distStr2[20] = "";
-	//PLL_Init();
 	UART0_Init();
 	InitRegisters();
   while(1){
